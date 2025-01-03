a7da84b279949703f0d1608241bf13ace862181f
Joram Kroon <joramkroon@live.nl>
2025-01-03 16:08:14 +0100

    refactor: optimize debounce delay and encoder handling for MIDI settings

--- Basic.ino
+++ Basic.ino
@@ -4,9 +4,9 @@
 // Constants
 const int NUM_CHANNELS = 8;
 const int MIDI_MAX_VALUE = 127;
-const int DEBOUNCE_DELAY = 50;
+const int DEBOUNCE_DELAY = 20;
 const int MIDI_BAUD_RATE = 31250;
-const unsigned long MODE_DISPLAY_DURATION = 1000; // 1 second
+const int THRESHOLD = 2;
 
 // Bar levels for LCD
 byte barLevels[8][8] = {
@@ -23,17 +23,13 @@ byte barLevels[8][8] = {
 const int A = 2;
 const int B = 3;
 const int C = 4;
-const int buttonPin = 7;
-const int threshold = 2;
-
-// Global variables
 Encoder myEnc(5, 6);
+const int buttonPin = 7;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 bool lastButtonState = false;
 unsigned long lastDebounceTime = 0;
-unsigned long modeChangeTime = 0;
-bool modeMessageDisplayed = false;
-int settingsMidiChannels[NUM_CHANNELS] = {0, 1, 2, 3, 4, 5, 6, 7};
+int settingsMidiChannels[NUM_CHANNELS][2] = {{1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 4}, {2, 3}};
+
 int lastPrintedValues[NUM_CHANNELS] = {-1, -1, -1, -1, -1, -1, -1, -1};
 int oldPosition = 0;
 int debouncedPosition = 0;
@@ -61,28 +57,26 @@ void loop()
   handleButtonPress();
   handleEncoder();
   updateDisplay();
-  mainFunction();
+  readAndSendMidiValues();
 }
 
 void handleButtonPress()
 {
   bool currentButtonState = digitalRead(buttonPin);
-
   if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY)
   {
     if (currentButtonState == LOW && lastButtonState == HIGH)
     {
-      selectedChannel = (selectedChannel + 1) % NUM_CHANNELS; // Cycle through channels
+      selectedChannel = (selectedChannel + 1) % NUM_CHANNELS;
       lcd.clear();
     }
   }
-
   lastButtonState = currentButtonState;
 }
 
 void handleEncoder()
 {
-  int newPosition = myEnc.read() / 4;
+  int newPosition = myEnc.read() / 3;
   if (newPosition != oldPosition)
   {
     lastEncoderDebounceTime = millis();
@@ -95,7 +89,7 @@ void handleEncoder()
     {
       debouncedPosition = oldPosition;
       int relativeChange = debouncedPosition - initialEncoderPosition;
-      settingsMidiChannels[selectedChannel] = constrain(settingsMidiChannels[selectedChannel] + relativeChange, 0, MIDI_MAX_VALUE);
+      settingsMidiChannels[selectedChannel][1] = constrain(settingsMidiChannels[selectedChannel][1] + relativeChange, 0, MIDI_MAX_VALUE);
       initialEncoderPosition = debouncedPosition;
     }
   }
@@ -106,10 +100,15 @@ void updateDisplay()
   static int lastDisplayedValues[NUM_CHANNELS] = {-1, -1, -1, -1, -1, -1, -1, -1};
   static int lastSelectedChannel = -1;
 
+  // Display MIDI channel in the top left corner
+  lcd.setCursor(1, 0);
+  lcd.print("MIDI");
+  lcd.print(settingsMidiChannels[selectedChannel][0]);
+
   // Display MIDI channel on the left side of the second row
-  lcd.setCursor(0, 1);
-  lcd.print("Ch ");
-  lcd.print(settingsMidiChannels[selectedChannel] + 1);
+  lcd.setCursor(1, 1);
+  lcd.print("CC");
+  lcd.print(settingsMidiChannels[selectedChannel][1] + 1);
   lcd.print("   "); // Clear any leftover characters
 
   for (int channel = 0; channel < NUM_CHANNELS; channel++)
@@ -126,14 +125,7 @@ void updateDisplay()
 
       // Display asterisk under the selected channel
       lcd.setCursor(col, 1);
-      if (channel == selectedChannel)
-      {
-        lcd.print("*");
-      }
-      else
-      {
-        lcd.print(" ");
-      }
+      lcd.print(channel == selectedChannel ? "*" : " ");
 
       lastDisplayedValues[channel] = barLevel;
     }
@@ -151,12 +143,12 @@ void selectChannel(int channel)
 
 void sendMIDIControlChange(int controller, int value)
 {
-  Serial.write(0xB0);
-  Serial.write(settingsMidiChannels[controller]);
+  Serial.write(0xB0 | (settingsMidiChannels[controller][0] - 1));
+  Serial.write(settingsMidiChannels[controller][1]);
   Serial.write(value);
 }
 
-void mainFunction()
+void readAndSendMidiValues()
 {
   int analogValues[NUM_CHANNELS];
 
@@ -167,7 +159,7 @@ void mainFunction()
 
     int midiValue = map(analogValues[channel], 0, 1023, 0, MIDI_MAX_VALUE);
 
-    if (abs(midiValue - lastPrintedValues[channel]) >= threshold)
+    if (abs(midiValue - lastPrintedValues[channel]) >= THRESHOLD)
     {
       lastPrintedValues[channel] = midiValue;
       sendMIDIControlChange(channel, midiValue);
